565e5f0aeef6cc306779e90d6eb4605ec0c7ed20
seyhpirr <baskan1904@hotmail.com>
2023-11-06 01:16:13 +0300

    schemas complete

--- server/db/schemas.sql
+++ server/db/schemas.sql
@@ -4,48 +4,40 @@
     password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE message(
-    id INTEGER UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    sender VARCHAR(255)  NOT NULL ,
-    chat_id VARCHAR(255) NOT NULL,
-    content TEXT,   
-    FOREIGN KEY(sender) REFERENCES user(username) ON DELETE CASCADE ,
-    FOREIGN KEY(chat_id) REFERENCES chat(id) ON DELETE CASCADE
-);
+
 
 CREATE TABLE session(
-    session_id VARCHAR(500) UNIQUE NOT NULL PRIMARY KEY,
+    session_id VARCHAR(255) UNIQUE NOT NULL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     FOREIGN KEY(username) REFERENCES user(username) ON DELETE CASCADE
 );
 
-CREATE TABLE chat_participance(
-    id VARCHAR(255) NOT NULL PRIMARY KEY,
+CREATE TABLE chat(
+    id VARCHAR(255) UNIQUE NOT NULL PRIMARY KEY,
+    chat_type ENUM("individual","group") NOT NULL
+);
+
+CREATE TABLE participance(
+    username VARCHAR(255) NOT NULL,
     chat_id VARCHAR(255) NOT NULL,
-    participant VARCHAR(255) NOT NULL,
-    FOREIGN KEY(chat_id) REFERENCES chat(id) ON DELETE CASCADE ,
-    FOREIGN KEY(participant) REFERENCES user(username) ON DELETE CASCADE 
+    is_admin BOOLEAN NOT NULL,
+    FOREIGN KEY(username) REFERENCES user(username),
+    FOREIGN KEY(chat_id) REFERENCES chat(id)
+
+);
+CREATE TABLE message(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    chat_id VARCHAR(255) NOT NULL,
+    sender VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    creation_time VARCHAR(255) NOT NULL
+    FOREIGN KEY(chat_id) REFERENCES chat(id),
+    FOREIGN KEY(sender) REFERENCES user(username)
 );
 
-CREATE TABLE group_chat(
-    id VARCHAR(255) NOT NULL PRIMARY KEY,
+CREATE TABLE group(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    chat_id VARCHAR(255) NOT NULL,
     group_name VARCHAR(255) NOT NULL,
-);
-
-
-CREATE TABLE group_participance(
-    id VARCHAR(255) NOT NULL PRIMARY KEY,
-    group_chat_id VARCHAR(255) NOT NULL,
-    participant VARCHAR(255) NOT NULL,
-    FOREIGN KEY(group_chat_id) REFERENCES group_chat(id) ON DELETE CASCADE ,
-    FOREIGN KEY(participant) REFERENCES user(username) ON DELETE CASCADE 
-);
-
-CREATE TABLE group_message(
-    id INTEGER UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    sender VARCHAR(255)  NOT NULL ,
-    group_chat_id VARCHAR(255) NOT NULL,
-    content TEXT,   
-    FOREIGN KEY(sender) REFERENCES user(username) ON DELETE CASCADE ,
-    FOREIGN KEY(group_chat_id) REFERENCES group(id) ON DELETE CASCADE
-);
+    FOREIGN KEY(chat_id) REFERENCES chat(id) 
+)
